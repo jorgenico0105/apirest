2b746d8722bf756de1c2a5e933bf913f0636799d
Nicolas <jorgengc1234@gmail.com>
2024-08-26 14:48:39 -0500

    Correccion ruta htpps

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -38,7 +38,7 @@ export default function useWeather() {
     })
     setProblem(false)
         try {
-            const geoUrl=`http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+            const geoUrl=`https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
             const {data} = await axios(geoUrl)
             // Comprobar si existe 
             if(!data[0]){
